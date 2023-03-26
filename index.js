71211760acecc9ac05ce0dadcfc8b4ee5bf3d5f2
Divyansh <divyanshraj0408@gmail.com>
2023-03-26 20:43:00 +0530

    Style changed

--- index.js
+++ index.js
@@ -11,56 +11,55 @@ if (leadsFromLocalStorage) {
     myleads = leadsFromLocalStorage
     render(myleads)
 }
-tabBtn.addEventListener("click", function() {
+tabBtn.addEventListener("click", function () {
     //chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {});
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         myleads.push(tabs[0].url)
-     
+
         localStorage.setItem("myleads", JSON.stringify(myleads))
         render(myleads)
     })
 })
 
-function deleteLink(index,leads){
-        const updatedLead = leads.splice(index,1)
-        localStorage.removeItem("myleads")
-        localStorage.setItem("myleads", JSON.stringify(leads))
-        render(leads)
-    }
+function deleteLink(index, leads) {
+    const updatedLead = leads.splice(index, 1)
+    localStorage.removeItem("myleads")
+    localStorage.setItem("myleads", JSON.stringify(leads))
+    render(leads)
+}
 
 function render(leads) {
     while (ulEl.firstChild) {
         ulEl.removeChild(ulEl.lastChild);
-      }
-    leads.forEach((lead,index)=>{
+    }
+    leads.forEach((lead, index) => {
         const li = document.createElement('li')
         const b = document.createElement('b')
         const a = document.createElement('a')
         const i = document.createElement('i')
         const hr = document.createElement('hr')
-        a.textContent=lead
-        i.addEventListener('click',()=>deleteLink(index,leads))
+        a.textContent = lead
+        i.addEventListener('click', () => deleteLink(index, leads))
         i.className = "fa fa-trash"
-        a.target= "_blank"
-        a.href= lead
+        a.target = "_blank"
+        a.href = lead
         b.textContent = `${index + 1}- `
         li.appendChild(b)
         li.appendChild(a)
         li.appendChild(i)
         li.appendChild(hr)
         ulEl.appendChild(li)
-        
     })
 }
 
-deleteBtn.addEventListener("dblclick", function() {
+deleteBtn.addEventListener("dblclick", function () {
     localStorage.clear()
     myleads = []
     render(myleads)
 })
 
-inputBtn.addEventListener("click", function() {
-    if(!inputEl.value){
+inputBtn.addEventListener("click", function () {
+    if (!inputEl.value) {
         return
     }
     myleads.push(inputEl.value)
